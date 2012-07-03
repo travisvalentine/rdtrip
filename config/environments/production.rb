a5b5470361ae413ed9f568a1cc780905c9723fb4
Travis Valentine <travis.valentine@livingsocial.com>
2012-07-03 00:06:20 -0400

    One more precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Roadtrip::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all
     # non-JS/CSS are already added)
-  config.assets.precompile << '*.js', 'mobile.css'
+  config.assets.precompile << '*.js'
+  config.assets.precompile << 'mobile.css'
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
