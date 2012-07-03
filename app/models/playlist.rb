5bf595dd4d35c6d255b0b8b0363296af46ec7b0c
Travis Valentine <travis.valentine@livingsocial.com>
2012-07-02 21:25:03 -0400

    Trying to figure out what's going on

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,7 +4,7 @@ class Playlist < ActiveRecord::Base
 
   def self.build_from_artist(artist, escape, recommender=Recommender)
     playlist = Playlist.find_or_create_by_escape_id(escape.id)
-    playlist.rdio_tracks += recommender.recommend_tracks_from_artist(artist)
+    logger.info (playlist.rdio_tracks += recommender.recommend_tracks_from_artist(artist)).to_s
     playlist.save
     playlist
   end
