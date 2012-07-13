a649133dd44e7eabe37434ed62b9665130d3e1b2
Travis Valentine <travis.valentine@livingsocial.com>
2012-07-13 16:20:10 -0400

    Reverse geocoding works

--- app/helpers/escapes_helper.rb
+++ app/helpers/escapes_helper.rb
@@ -6,4 +6,13 @@ module EscapesHelper
     	tag('meta', name: 'playback_token', content: RDIO.getPlaybackToken(domain: "localhost"))
     end
   end
+
+  def yahoo_app_id
+  	if Rails.env.production?
+  		tag('meta', name: 'yahoo_id', content: ENV['YAHOO_GEO_ID'])
+  	else
+  		tag('meta', name: 'yahoo_id', content: YAHOO_GEO_ID)
+  	end
+  end
+
 end
